091990f27626c7b6ce5f563513508c8fd5c9286c
Alissa <alissa@luepke.us>
2017-06-19 21:33:27 +0200

    design

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,15 @@
+
+// AJAX Request --> Holen der Bilder
 $.ajax({
-    url: "/dir",
-    method: "GET"
-    
-}).done(function(message){
-  for(var x in message){
-      $.ajax({
-          url: "/photos/"+message[x],
-          method: "GET"
-      }).done(function(message){
-          $("#gallery").append("<img src='"+decodeURIComponent(message)+"'>");
-      })
-      
-  }
-    
-    
+    url: "/dir"
+    , method: "GET"
+}).done(function (message) {
+    for (var x in message) {
+        $.ajax({
+            url: "/photos/" + message[x]
+            , method: "GET"
+        }).done(function (message) {
+            $("#gallery").append("<img src='" + decodeURIComponent(message) + "'>");
+        })
+    }
 });
