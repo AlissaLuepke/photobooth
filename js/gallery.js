940cb0c5fa0954a607976eb4f353b03b82ba9825
Tobias Wendzel <too.low@gmx.net>
2017-06-25 19:17:30 +0200

    Made gallery dynamic

--- js/gallery.js
+++ js/gallery.js
@@ -1,55 +1,94 @@
-var carousel = $("#carousel").flipster({
-    style: 'carousel',
-    spacing: -0.5,
-    nav: false,
-    buttons: true,
-    loop: true,
-    
-    start: 0
-});
+var carousel;
+var timeline;
+var counter = 0;
+var last_year = 0;
+var last_month = 0;
+var last_day = 0;
+var last_hour = 0;
 
+var months = ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"];
 
-$("#flat").flipster({
-    style: 'flat',
-    spacing: -0.15,
-   
-    start: 0
-});
 // AJAX Request --> Holen der Bilder
 $.ajax({
     url: "/dir",
     method: "GET"
     
 }).done(function(message){
-  for(var x in message){
-      $.ajax({
-          url: "/photos/"+message[x],
-          method: "GET"
-      }).done(function(message) {
-      	 var temp = "<li class='gallery_image'>";
-          temp += "<img src='"+decodeURIComponent(message.img)+"'>";
-          temp += "<p class='font_" + message.font + "'>" + decodeURIComponent(decodeURI(message.text)) + "</p>";
-          temp += "</li>";
-          $('#gallery').append(temp);
-      });
-      
-  }
-    
-    
+	  
+	  for(var x in message){
+	      
+	      $.ajax({
+		  url: "/photos/"+message[x],
+		  method: "GET",
+		  async: false	// die synchrone Ausführung sorgt hier dafür, dass die per Dateiname schon geordnete Reihenfolge erhalten bleibt
+	      
+	      }).done(function(message2) {
+
+		var date = message[x].split(".")[0].split("_");
+		var year = date[0];
+		var month = date[1];
+		var day = date[2];
+		var hour = date[3];
+		var minute = date[4];
+		
+		if (+year > last_year ) {
+			last_year = +year;
+			last_month = 0;
+			appendTimeList( counter, year);
+		}
+		if (+month > last_month ) {
+			last_month = +month;
+			last_day = 0;
+			appendTimeList( counter, months[+month -1]);
+		}
+		if (+day > last_day ) {
+			last_day = +day;
+			last_hour = 0;
+			appendTimeList( counter, day + ".");
+		}
+		if (+hour > last_hour ) {
+			last_hour = +hour;
+			appendTimeList( counter, (+hour < 10 ? "0" + hour : hour) + ":00");
+		}
+
+	      	var temp = "<li class='gallery_image'>";
+		temp += "<img class='gallerysize' src='"+decodeURIComponent(message2.img)+"'>";
+		temp += "<p class='font_" + message2.font + "'>" + decodeURIComponent(decodeURI(message2.text)) + "</p>";
+		temp += "<p class='date_text'>" + day + "." + month + "." + year + " " + hour + ":" + minute  + "</p>";
+		temp += "</li>";
+		$('#carousel_list').append(temp);
+		counter++;
+		
+		if ( counter == message.length ) {
+		  	carousel = $("#carousel").flipster({
+			    style: 'carousel',
+			    spacing: -0.5,
+			    nav: false,
+			    buttons: true,
+			    loop: true,
+			    
+			    start: 0
+			});
+			
+			timeline = $("#flat").flipster({
+			    style: 'flat',
+			    spacing: -0.15,
+			   
+			    start: 0
+			});
+			
+			$('.galleryfoldersize').click(function(){
+			    	carousel.flipster('jump', +$(this).attr("imageID")); // Jump to a specific index
+			});
+		  }
+	      });
+	  }
+	  
 });
 
-$('#0').click(function(){
-   
-    
-    carousel.flipster('jump', 0); // Jump to a specific index
-});
-
-
-$('#1').click(function(){
-   
-    
-    carousel.flipster('jump', 2); // Jump to a specific index
-});
+function appendTimeList(id, text) {
+	$('#flat_list').append('<li><div class="galleryfoldersize" imageID=' + id +' > <p class="foldertext"> ' + text + ' </p></div></li>');
+}
 
 
 
