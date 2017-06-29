9a5eadb06da66b5a811cb1f9736687292dcefda9
Alissa <alissa@luepke.us>
2017-06-29 16:59:01 +0200

    save-delete-button

--- js/landingpage.js
+++ js/landingpage.js
@@ -1,3 +1,9 @@
+
+$(document).ready(function () {
+		$('#titletext').css("transition", "letter-spacing 3s");
+		$('#titletext').css("letter-spacing", "0px");
+    	});
+
 $('.Infobutton').click(function(){
 
     $('.infowindow-hidden').toggleClass('infowindow');
